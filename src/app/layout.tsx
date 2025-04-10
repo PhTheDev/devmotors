65e0e26ea4447fb77ba0592f974f86d2eb6f7dbe
PhTheDev <fariagm123@gmail.com>
2025-04-10 14:49:02 -0300

    Enhance metadata in layout.tsx with keywords, authors, Open Graph properties, and robots directives for improved SEO and social sharing.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "DevMotors - Sua oficina especializada!",
   description: "Oficina de carros em Goiânia",
+  keywords: ["oficina", "carros", "reparos", "manutenção", "venda de peças"],
+  authors: [{ name: "PhTheDev", url: "https://github.com/PhTheDev/" }],
+  openGraph: {
+    title: "DevMotors - Sua oficina especializada!",
+    description: "Oficina de carros em Goiânia",
+    url: "https://github.com/PhTheDev/",
+    siteName: "DevMotors",
+    images: [`${process.env.NEXT_PUBLIC_URL}/logo.jpg`],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
